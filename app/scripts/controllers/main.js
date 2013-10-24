4b5efc15d6f3c4ff4a45ab741360044d7640e8b1
Maxime Vidori <maxime.vidori@enovance.com>
2013-10-24 10:36:01 +0200

    Initialization

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('horizonMobileApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
