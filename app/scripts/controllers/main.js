14631a426df29c43316cc2c8bb2f2b5d62ea6d03
Maxime Vidori <maxime.vidori@enovance.com>
2013-10-25 17:57:40 +0200

    reuse angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
-'use strict';
-
+/*global angular, $*/
 angular.module('horizonMobileApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function () {
+    'use strict';
+
+    $('.menu-elem').css({
+      "height": function () {
+        return $(this).width();
+      }
+    }).children('.glyphicon').css(
+      "font-size",
+      function () {
+        return $(this).parent().width() / 2;
+      }
+    );
   });
